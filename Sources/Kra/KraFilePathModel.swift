ff6303cda4af9771d9b50617b38003242e3cea8d
MasterBlaster Code <odhlasenia_vlady0x@icloud.com>
2025-05-04 18:41:30 +0200

    Update KraFilePathModel.swift

--- Sources/Kra/KraFilePathModel.swift
+++ Sources/Kra/KraFilePathModel.swift
@@ -33,31 +33,23 @@ public struct KraFilePathModel: Codable {
         public init(from decoder: Decoder) throws {
             let container = try decoder.singleValueContainer()
             
-            // Najprv skúsime dekódovať ako zoznam, pretože podľa vášho výpisu to vyzerá ako pole
+            // Najprv skúsime dekódovať ako zoznam, pretože podľa výpisu to vyzerá ako pole
             do {
                 let fileList = try container.decode([FileInfo].self)
                 self = .list(fileList)
+                return
             } catch {
                 // Ak to nefunguje, skúsime dekódovať ako jednotlivý súbor
                 do {
                     let singleFile = try container.decode(KraFilePathData.self)
                     self = .single(singleFile)
-                } catch let error as DecodingError {
-                    // Spracovanie chyby dekódovania
-                    switch error {
-                    case .keyNotFound(let key, let context):
-                        print("Kľúč \(key.stringValue) nebol nájdený: \(context.debugDescription)")
-                    case .typeMismatch(let type, let context):
-                        print("Očakávaný typ \(type) sa nezhoduje: \(context.debugDescription)")
-                    case .valueNotFound(let type, let context):
-                        print("Hodnota typu \(type) nebola nájdená: \(context.debugDescription)")
-                    case .dataCorrupted(let context):
-                        print("Poškodené dáta: \(context.debugDescription)")
-                    @unknown default:
-                        print("Neznáma chyba dekódovania")
-                    }
+                    return
                 } catch {
-                    print("Iná chyba: \(error)")
+                    // Ak ani to nefunguje, vyhodíme chybu
+                    throw DecodingError.dataCorruptedError(
+                        in: container,
+                        debugDescription: "Data is neither a single file nor a file list: \(error.localizedDescription)"
+                    )
                 }
             }
         }
@@ -73,10 +65,56 @@ public struct KraFilePathModel: Codable {
             }
         }
     }
+    
+    // Vlastný inicializátor pre lepšie spracovanie chýb
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        // Dekódujeme základné vlastnosti
+        msg = try container.decodeIfPresent(String.self, forKey: .msg)
+        success = try container.decode(Int.self, forKey: .success)
+        
+        // Dekódujeme data podľa typu
+        data = try container.decode(FileData.self, forKey: .data)
+    }
+    
+    // Pomocná metóda pre vytvorenie modelu zo surových dát
+    public static func decode(from data: Data) throws -> KraFilePathModel {
+        let decoder = JSONDecoder()
+        do {
+            return try decoder.decode(KraFilePathModel.self, from: data)
+        } catch {
+            print("Chyba dekódovania KraFilePathModel: \(error)")
+            throw error
+        }
+    }
+    
+    // Pomocná metóda pre ladenie
+    public static func printJSON(_ data: Data) {
+        if let jsonString = String(data: data, encoding: .utf8) {
+            print("JSON: \(jsonString)")
+        } else {
+            print("Nepodarilo sa previesť dáta na reťazec")
+        }
+    }
 }
 
 public struct KraFilePathData: Codable {
     public let link: String
+    
+    // Pridané pre prípad, že API vráti ďalšie polia
+    private enum CodingKeys: String, CodingKey {
+        case link
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        link = try container.decode(String.self, forKey: .link)
+    }
+    
+    public init(link: String) {
+        self.link = link
+    }
 }
 
 public struct FileInfo: Codable {
@@ -87,4 +125,64 @@ public struct FileInfo: Codable {
     public let folder: Bool
     public let created: String
     public let password: Bool
+    
+    // Pridané pre prípad, že API vráti ďalšie polia
+    private enum CodingKeys: String, CodingKey {
+        case size, name, shared, ident, folder, created, password
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        size = try container.decode(Int.self, forKey: .size)
+        name = try container.decode(String.self, forKey: .name)
+        shared = try container.decode(Bool.self, forKey: .shared)
+        ident = try container.decode(String.self, forKey: .ident)
+        folder = try container.decode(Bool.self, forKey: .folder)
+        created = try container.decode(String.self, forKey: .created)
+        password = try container.decode(Bool.self, forKey: .password)
+    }
+    
+    public init(size: Int, name: String, shared: Bool, ident: String, folder: Bool, created: String, password: Bool) {
+        self.size = size
+        self.name = name
+        self.shared = shared
+        self.ident = ident
+        self.folder = folder
+        self.created = created
+        self.password = password
+    }
+}
+
+// Rozšírenie pre jednoduchšie použitie
+extension KraFilePathModel {
+    // Získa prvý súbor zo zoznamu, ak existuje
+    public var firstFile: FileInfo? {
+        return files?.first
+    }
+    
+    // Skontroluje, či odpoveď obsahuje zoznam súborov
+    public var isListResponse: Bool {
+        if case .list(_) = data {
+            return true
+        }
+        return false
+    }
+    
+    // Skontroluje, či odpoveď obsahuje jeden súbor
+    public var isSingleFileResponse: Bool {
+        if case .single(_) = data {
+            return true
+        }
+        return false
+    }
+    
+    // Vráti počet súborov v zozname alebo 0, ak nejde o zoznam
+    public var fileCount: Int {
+        return files?.count ?? 0
+    }
+    
+    // Vráti identifikátor prvého súboru alebo nil
+    public var firstFileIdent: String? {
+        return firstFile?.ident
+    }
 }
