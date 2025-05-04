a35a5a6e02878776fbff76c0dde8f81960307dd2
MasterBlaster Code <odhlasenia_vlady0x@icloud.com>
2025-05-04 14:02:39 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,11 @@ import PackageDescription
 let package = Package(
     name: "Kra",
     platforms: [
-        .macOS(.v10_13),
-        .iOS(.v12)
+        .macOS(.v12),
+        .iOS(.v15),
+        .tvOS(.v15),
+        .macCatalyst(.v15),
+        .custom("visionos", versionString: "1.0")
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
