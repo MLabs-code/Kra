5eb03026266406358de5207a77fc3a7a30f662b2
MasterBlaster Code <odhlasenia_vlady0x@icloud.com>
2025-05-04 18:34:17 +0200

    Update KraFilePathModel.swift

--- Sources/Kra/KraFilePathModel.swift
+++ Sources/Kra/KraFilePathModel.swift
@@ -9,15 +9,66 @@ import Foundation
 
 public struct KraFilePathModel: Codable {
     public let msg: String?
-    public let data: KraFilePathData?
     public let success: Int
+    public let data: FileData
     
     public var urlLink: URL? {
-        URL(string: data?.link ?? "")
+        if case .single(let fileData) = data {
+            return URL(string: fileData.link)
+        }
+        return nil
+    }
+    
+    public enum FileData: Codable {
+        case single(KraFilePathData)
+        case list([FileInfo])
+        
+        public init(from decoder: Decoder) throws {
+            let container = try decoder.singleValueContainer()
+            
+            // Skúsime dekódovať ako jednotlivý súbor
+            do {
+                let singleFile = try container.decode(KraFilePathData.self)
+                self = .single(singleFile)
+                return
+            } catch {
+                // Ak to nefunguje, skúsime dekódovať ako zoznam
+                do {
+                    let fileList = try container.decode([FileInfo].self)
+                    self = .list(fileList)
+                    return
+                } catch {
+                    throw DecodingError.dataCorruptedError(
+                        in: container,
+                        debugDescription: "Data is neither a single file nor a file list"
+                    )
+                }
+            }
+        }
+        
+        public func encode(to encoder: Encoder) throws {
+            var container = encoder.singleValueContainer()
+            
+            switch self {
+            case .single(let fileData):
+                try container.encode(fileData)
+            case .list(let fileList):
+                try container.encode(fileList)
+            }
+        }
     }
 }
 
-// MARK: - DataClass
 public struct KraFilePathData: Codable {
     public let link: String
+}
+
+public struct FileInfo: Codable {
+    public let size: Int
+    public let name: String
+    public let shared: Bool
+    public let ident: String
+    public let folder: Bool
+    public let created: String
+    public let password: Bool
 }
